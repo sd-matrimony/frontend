5e66a27a4c57329f38aac1cbce10dd7f4e7222ba
Raj kumar <glrajv369@gmail.com>
2026-08-30 19:32:27 +0530

    fix: build

--- src/app/admin/make-match/page.tsx
+++ src/app/admin/make-match/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import MakeMatch from "@/components/admin/make-match";
 
 function Page() {
-  return <MakeMatch />
+  return (
+    <Suspense>
+      <MakeMatch />
+    </Suspense>
+  )
 }
 
 export default Page
